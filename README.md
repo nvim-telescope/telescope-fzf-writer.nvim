edb633d45fbf62de57bfdd0cb27127764320febd
TJ DeVries <devries.timothyj@gmail.com>
2020-11-23 12:31:39 -0500

    feat: Fix greps and remove sorters

--- README.md
+++ README.md
@@ -26,6 +26,11 @@ require('telescope').setup {
         fzf_writer = {
             minimum_grep_characters = 2,
             minimum_files_characters = 2,
+
+            -- Disabled by default.
+            -- Will probably slow down some aspects of the sorter, but can make color highlights.
+            -- I will work on this more later.
+            use_highlighter = true,
         }
     }
 }
--- lua/telescope/_extensions/fzf_writer.lua
+++ lua/telescope/_extensions/fzf_writer.lua
@@ -5,11 +5,14 @@ local finders = require('telescope.finders')
 local make_entry = require('telescope.make_entry')
 local pickers = require('telescope.pickers')
 local previewers = require('telescope.previewers')
+local sorters = require('telescope.sorters')
 
 local flatten = vim.tbl_flatten
 
 local minimum_grep_characters = 2
-local minimum_files_characters = 2
+local minimum_files_characters = 0
+
+local use_highlighter = false
 
 return require('telescope').register_extension {
   setup = function(conf)
@@ -20,15 +23,18 @@ return require('telescope').register_extension {
     if conf.minimum_files_characters then
       minimum_files_characters = conf.minimum_files_characters
     end
+
+    if conf.use_highlighter ~= nil then
+      use_highlighter = conf.use_highlighter
+    end
   end,
 
   exports = {
     grep = function(opts)
       opts = opts or {}
 
-      local _ = make_entry.gen_from_vimgrep(opts)
       local live_grepper = finders._new {
-        fn_command = function(self, prompt)
+        fn_command = function(_, prompt)
           if not prompt or prompt == "" then
             return nil
           end
@@ -37,7 +43,8 @@ return require('telescope').register_extension {
             return nil
           end
 
-          local rg_args = flatten { conf.vimgrep_arguments }
+          local rg_args = flatten { conf.vimgrep_arguments, "." }
+          table.remove(rg_args, 1)
 
           return {
             writer = Job:new {
@@ -46,19 +53,18 @@ return require('telescope').register_extension {
             },
 
             command = 'fzf',
-            args = {'--filter', prompt}
+            args = {'--filter', prompt},
           }
         end,
 
         entry_maker = make_entry.gen_from_vimgrep(opts),
-
       }
 
       pickers.new(opts, {
         prompt_title = 'Live Grep',
         finder = live_grepper,
         previewer = previewers.vimgrep.new(opts),
-        sorter = conf.generic_sorter(opts),
+        sorter = use_highlighter and sorters.highlighter_only(opts),
       }):find()
     end,
 
@@ -91,7 +97,7 @@ return require('telescope').register_extension {
         prompt_title = 'Live Grep',
         finder = live_grepper,
         previewer = previewers.vimgrep.new(opts),
-        -- sorter = conf.generic_sorter(opts),
+        sorter = use_highlighter and sorters.highlighter_only(opts),
       }):find()
     end,
   },
